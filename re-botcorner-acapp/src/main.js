c41e714bea2e2b97c61cf4693fa68a1e08e16c23
Andrew Leung <84608230+SokuRitszZ@users.noreply.github.com>
2023-07-13 09:47:29 +0800

    Delete re-botcorner-acapp directory

--- re-botcorner-acapp/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import { createPinia } from 'pinia';
-
-import 'animate.css';
-
-const app = createApp(App);
-app.use(createPinia()); /** pinia */
-app.mount('#app');
